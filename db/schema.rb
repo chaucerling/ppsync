891a0355df531a4132a46ba8b86e5397a95c927b
chaucerling <chaucerling@gmail.com>
2015-01-30 13:06:28 +0800

    modify database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150128073924) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "catalogs", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "name"
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 20150128073924) do
     t.datetime "updated_at",        null: false
   end
 
-  add_index "user_websites", ["user_id", "system_website_id"], name: "index_user_websites_on_user_id_and_system_website_id", unique: true
+  add_index "user_websites", ["user_id", "system_website_id"], name: "index_user_websites_on_user_id_and_system_website_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -66,7 +69,7 @@ ActiveRecord::Schema.define(version: 20150128073924) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
