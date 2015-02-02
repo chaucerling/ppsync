d2b5733cad0591afe3ab1943f70a93af33918501
chaucerling <chaucerling@gmail.com>
2015-02-02 19:30:21 +0800

    ignore qiniu_sdk

--- config/initializers/qiniu_sdk.rb
+++
@@ -1,4 +0,0 @@
-require 'qiniu'
-
-Qiniu.establish_connection! :access_key => 'QPg0_gqzPQPzZZCUm3Um6WxxwKluYzkxnxevc3cQ',
-                                            :secret_key => 'wlZaFm2eEj2SdQB0SpRVPcI71G-kpuc0bBPfO0-7'
